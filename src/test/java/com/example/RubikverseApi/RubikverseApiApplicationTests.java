373ec3616a6096f5a7e58927f53d50e8f8656ba7
estelaV9 <esteladeveg@gmail.com>
2025-11-25 19:41:03 +0100

    Añadir proyecto Spring Boot RubikverseApi

---
+++ src/test/java/com/example/RubikverseApi/RubikverseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RubikverseApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubikverseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
